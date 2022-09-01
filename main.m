c3dbf005667a2c453f22c1aa3b771f13ebe82d8c
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2022-09-01 13:24:40 +0200

    Update main.m

--- main.m
+++ main.m
@@ -22,74 +22,13 @@ deltaT = 0 ; %K
 
 %% PREPROCESS
 
-% Nodal coordinates matrix creation
-%  x(a,j) = coordinate of node a in the dimension j
-x = [%  X       Y
-0 0 ;
-0.5 0.2 ;
-1 0.4 ;
-1.5 0.6 ;
-0 0.5 ;
-0.5 0.6 ;
-1 0.7 ;
-1.5 0.8
-];
-
-% Connectivities matrix ceation
-%  Tn(e,a) = global nodal number associated to node a of element e
-Tn = [%     a      b
-1 2 ;
-2 3 ;
-3 4 ;
-5 6 ;
-6 7 ; 
-7 8 ;
-1 5 ;
-1 6 ; 
-2 5 ;
-2 6 ; 
-2 7 ;
-3 6 ;
-3 7 ;
-3 8 ;
-4 7 ;
-4 8 ;
-];
-
-% External force matrix creation
-%  Fdata(k,1) = node at which the force is applied
-%  Fdata(k,2) = DOF (direction) at which the force is applied
-%  Fdata(k,3) = force magnitude in the corresponding DOF
-Fdata = [%   Node        DOF  Magnitude   
-2 4 3*F ;
-3 6 2*F ;
-4 8 F ;
-];
-
-% Fix nodes matrix creation
-%  fixNod(k,1) = node at which some DOF is prescribed
-%  fixNod(k,2) = DOF prescribed
-%  fixNod(k,3) = prescribed displacement in the corresponding DOF (0 for fixed)
-fixNod = [% Node        DOF  Magnitude
-1 1 0 ;
-1 2 0 ;
-5 9 0 ;
-5 10 0 ;
-];
-
-% Material data
-%  mat(m,1) = Young modulus of material m
-%  mat(m,2) = Section area of material m
-%  --more columns can be added for additional material properties--
-mat = [% Young M.   Section A.   thermal_coeff   Inertia
-         Young,   Area,      thermal_coeff,     Inertia;  % Material (1)
-];
-
-% Material connectivities
-%  Tmat(e) = Row in mat corresponding to the material associated to element e 
-Tmat = [% Mat. index
-1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1 ; 1  
-];
+s.F = F ;
+s.Young = Young ;
+s.Area = Area ;
+s.thermal_coeff = thermal_coeff ;
+s.Inertia = Inertia ;
+c = PreprocessComputer(s) ;
+[x,Tn,Fdata,fixNod,mat,Tmat] = c.compute() ;
 
 %% SOLVER
 
