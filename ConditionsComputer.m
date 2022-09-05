cd659796205a5aceb0815795c8cbab1ae7fc2864
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2022-09-05 11:19:29 +0200

    vR,uR,vL

--- ConditionsComputer.m
+++ ConditionsComputer.m
@@ -11,25 +11,37 @@ classdef ConditionsComputer < handle
         end
 
         function [vL,vR,uR] = compute(obj)
+            vL = obj.computeFreeDOF();
+            vR = obj.computePrescDOF() ;
+            uR = obj.computePrescDispl() ;
             
+        end
+    end
+
+    methods (Access = private)
+        function init(obj,cParams)
+            obj.n_dof = cParams.n_dof ;
+            obj.fixNod = cParams.fixNod ;
+        end
+
+        function prescribed_dofs = computePrescribed_dofs(obj)
+            fixedNodes = obj.fixNod ;
+            prescribed_dofs = size(fixedNodes,1) ;
+        end
+        
+        function vL = computeFreeDOF(obj)
             nDim = obj.n_dof ;
             fixedNodes = obj.fixNod ;
-
-            prescribed_dofs = size(fixedNodes,1) ;
-
+            prescribed_dofs = obj.computePrescribed_dofs() ;
             vL = zeros(nDim-prescribed_dofs,1) ;
-            vR = zeros(prescribed_dofs,1) ;
-            uR = zeros(prescribed_dofs,1) ;
-            
+
             a = 0 ;
             pos_vL = 1 ;
-            
+
             for i = 1:nDim
                 for j = 1:prescribed_dofs
                     if i == fixedNodes(j,2)
                         a = 1 ;
-                        vR(j) = i ;
-                        uR(j) = fixedNodes(j,3) ;
                     end
                 end
                 if a == 0
@@ -39,13 +51,53 @@ classdef ConditionsComputer < handle
                 a = 0 ;
             end
         end
-    end
 
-    methods (Access = private)
-        function init(obj,cParams)
-            obj.n_dof = cParams.n_dof ;
-            obj.fixNod = cParams.fixNod ;
+        function vR = computePrescDOF(obj)
+            nDim = obj.n_dof ;
+            fixedNodes = obj.fixNod ;
+            prescribed_dofs = obj.computePrescribed_dofs() ;
+            vR = zeros(prescribed_dofs,1) ;
+            
+            a = 0 ;
+            pos_vL = 1 ;
+
+            for i = 1:nDim
+                for j = 1:prescribed_dofs
+                    if i == fixedNodes(j,2)
+                        a = 1 ;
+                        vR(j) = i ;
+                    end
+                end
+                if a == 0
+                    pos_vL = pos_vL + 1 ; 
+                end
+                a = 0 ;
+            end
         end
+
+        function uR = computePrescDispl(obj)
+            nDim = obj.n_dof ;
+            fixedNodes = obj.fixNod ;
+            prescribed_dofs = obj.computePrescribed_dofs() ;
+            uR = zeros(prescribed_dofs,1) ;
+
+            a = 0 ;
+            pos_vL = 1 ;
+
+            for i = 1:nDim
+                for j = 1:prescribed_dofs
+                    if i == fixedNodes(j,2)
+                        a = 1 ;
+                        uR(j) = fixedNodes(j,3) ;
+                    end
+                end
+                if a == 0
+                    pos_vL = pos_vL + 1 ; 
+                end
+                a = 0 ;
+            end
+        end
+
     end
 
 end
