195e1821f08126ea680f67200f94330ab6cd72f9
Laura <laura.dominguez.perez@estudiantat.upc.edu>
2022-09-20 08:42:37 +0200

    changes

--- SolverPreparationComputer.m
+++ SolverPreparationComputer.m
@@ -1,19 +1,72 @@
 classdef SolverPreparationComputer < handle
-    properties
-        Property1
+    properties (Access = private)
+        vL
+        vR
+        uR
+        KG
+        Fext
+        solverType
+    end
+    properties (Access = private)
+        splittedK
+        extF
+        uLdisp
     end
 
-    methods
-        function obj = untitled2(inputArg1,inputArg2)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+    methods (Access = public)
+        function obj = SolverPreparationComputer(cParams)
+            obj.init(cParams) ;
         end
 
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function [K,F,uL] = compute(obj)
+            obj.splitStiffnessMatrix() ;
+            obj.createFext() ;
+            obj.computeFreeDisp() ;
+            K = obj.splittedK ;
+            F = obj.extF ;
+            uL = obj.uLdisp ;
+        end
+    end
+
+    methods (Access = private)
+        function init(obj,cParams)
+            obj.vL = cParams.vL ;
+            obj.vR = cParams.vR ;
+            obj.uR = cParams.uR ;
+            obj.KG = cParams.KG ;
+            obj.Fext = cParams.Fext ;
+            obj.solverType = cParams.solverType;
+        end
+
+        function splitStiffnessMatrix(obj)
+            freeDOF = obj.vL ;
+            prescribedDOF = obj.vR ;
+            StiffMat = obj.KG;
+            K.KLL = StiffMat(freeDOF,freeDOF) ;
+            K.KLR = StiffMat(freeDOF,prescribedDOF) ;
+            K.KRL = StiffMat(prescribedDOF,freeDOF) ;
+            K.KRR = StiffMat(prescribedDOF,prescribedDOF) ;
+            obj.splittedK = K ;
+        end
+
+        function createFext(obj)
+            freeDOF = obj.vL ;
+            prescribedDOF = obj.vR ;
+            Forces = obj.Fext;
+            F.Fext_L = Forces(freeDOF,1) ;
+            F.Fext_R = Forces(prescribedDOF,1) ;
+            obj.extF = F ;
+        end
+
+        function computeFreeDisp(obj)
+            obj.splitStiffnessMatrix() ;
+            obj.createFext() ;
+            prescribedDispl = obj.uR ;
+            K = obj.splittedK ;
+            F = obj.extF ;
+            solver = Solver.chooseMode(obj.solverType);
+            uL = solver.system((F.Fext_L-K.KLR*prescribedDispl),K.KLL) ;
+            obj.uLdisp = uL ;
         end
     end
 end
